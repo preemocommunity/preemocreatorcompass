911f6455d7577cce99a4425dcd248de56e7ad0fc
Prëmo <95042253+preemocommunity@users.noreply.github.com>
2026-04-12 22:02:50 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
